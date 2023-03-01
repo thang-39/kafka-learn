3b1e46d2c81547b53292fa0cd3ba9df3334b5110
thang-39 <91838559+thang-39@users.noreply.github.com>
2023-03-01 23:33:05 +0700

    Custom handler exception for validation , unit test for this validation

--- src/test/java/unit/com/learnkafka/controller/LibraryEventControllerUnitTest.java
+++ src/test/java/unit/com/learnkafka/controller/LibraryEventControllerUnitTest.java
@@ -15,6 +15,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(LibraryEventsController.class)
@@ -46,13 +47,56 @@ public class LibraryEventControllerUnitTest {
 
         doNothing().when(libraryEventProducer).sendLibraryEvent_Approach2(isA(LibraryEvent.class));
 
-        // when
+        // expect
         mockMvc.perform(post("/v1/libraryevent")
                         .content(json)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
+    }
 
-        // then
+    @Test
+    void postLibraryEvent_BookIsNull_4xx() throws Exception {
+        // given
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                .libraryEventId(1)
+                .book(null)
+                .build();
+        String json = objectMapper.writeValueAsString(libraryEvent);
+
+        doNothing().when(libraryEventProducer).sendLibraryEvent_Approach2(isA(LibraryEvent.class));
+
+        // expect
+        mockMvc.perform(post("/v1/libraryevent")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+
+    }
+
+    @Test
+    void postLibraryEvent_bookHasNullFields_4xx() throws Exception {
+        // given
+        Book book = Book.builder()
+                .bookId(123)
+                .bookAuthor(null)
+                .bookName(null)
+                .build();
+
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                .libraryEventId(1)
+                .book(book)
+                .build();
+        String json = objectMapper.writeValueAsString(libraryEvent);
+
+        doNothing().when(libraryEventProducer).sendLibraryEvent_Approach2(isA(LibraryEvent.class));
+
+        // expect
+        String expectedErrorMessage = "book.bookAuthor - must not be blank, book.bookName - must not be blank";
+        mockMvc.perform(post("/v1/libraryevent")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().string(expectedErrorMessage));
 
     }
 }
